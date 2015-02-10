b9b8542bdfbad7567cca157ebaa935e761cd4f5f
Brandon Huynh <yuki@ucrwpa2-3-178-180.bulk.ucr.edu>
2015-02-09 18:39:33 -0800

    outlined rugby execution framework

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .rugby import Rugby
---
+++ run_playbooks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script runs sets VAGRANT_CWD and runs vagrant up
+
+# This is the location of the Vagrantfile to use
+location="/Users/yuki/TEST"
+
+
+#export VAGRANT_CWD="$location"
+#echo $VAGRANT_CWD
+#vagrant up
+
+echo "This script does nothing yet..."
