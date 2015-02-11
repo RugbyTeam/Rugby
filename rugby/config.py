ba529a7d418ea9928faa51af3b64167d70609caf
Alex Gonzales <agonz056@ucr.edu>
2015-02-11 13:48:52 -0800

    added list of valid groups and types

--- rugby/config.py
+++ rugby/config.py
@@ -7,9 +7,15 @@ VM_STATIC_IPS = {
     'lang' : '192.168.0.16'
 }
 
+GROUPS = ['lang', 'db']
+GROUP_TO_TYPES = { 
+    'db'   : ['mongo'],
+    'lang' : ['node'] 	   
+}
+TYPES = sorted({vm_type for vm_types in GROUP_TO_TYPES.itervalues() for vm_type in vm_types})
+
 # Rugby constants
 SITE_YML = '/opt/Rugby-Playbooks/site.yml'
 SCRIPTS_DIR = abspath(join(dirname(__file__), 'shell_scripts'))
 TEMPLATES_DIR = abspath(join(dirname(__file__), 'templates'))
 VAGRANT_TEMPLATE_FILE = join(TEMPLATES_DIR, 'Vagrantfile.j2')
-
