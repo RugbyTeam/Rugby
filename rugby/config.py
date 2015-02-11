c6b8043faff6535027c9e62eb937809de8aea541
omoman <mannysanchez998@gmail.com>
2015-02-11 09:09:20 -0800

    Fixed config paths and added a couple extra paths

--- rugby/config.py
+++ rugby/config.py
@@ -2,8 +2,14 @@ from os.path import dirname, abspath, join
 
 # Virtual machine related constants
 VM_BASE_DIR = '/opt/VMs'
+VM_STATIC_IPS = {
+    'db'   : '192.168.0.15',
+    'lang' : '192.168.0.16'
+}
 
 # Rugby constants
-SCRIPTS_DIR = dirname(abspath(join(__file__, 'shell_scripts')))
-TEMPLATES_DIR = dirname(abspath(join(__file__, 'templates')))
+SITE_YML = '/opt/Rugby-Playbooks/site.yml'
+SCRIPTS_DIR = abspath(join(dirname(__file__), 'shell_scripts'))
+TEMPLATES_DIR = abspath(join(dirname(__file__), 'templates'))
+VAGRANT_TEMPLATE_FILE = join(TEMPLATES_DIR, 'Vagrantfile.j2')
 
