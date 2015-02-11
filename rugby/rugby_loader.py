d75a7342a297bea19b2b4423ca8ff2c745691d47
Alex Gonzales <agonz056@ucr.edu>
2015-02-11 13:46:45 -0800

    added validations to rugby_loader

--- rugby/rugby_loader.py
+++ rugby/rugby_loader.py
@@ -3,10 +3,52 @@ import config
 
 # external 
 import yaml
-from jinja2 import Template
+import pyrx
+import jinja2
 
 # stdlib
 import os
+
+# import contants from configuration as global variables
+valid_groups = config.GROUPS
+valid_types  = config.TYPES
+valid_group_to_types = config.GROUP_TO_TYPES
+
+# Where vagrantfile template is located
+vagrant_template_file = config.VAGRANT_TEMPLATE_FILE
+        
+# Exception class that is thrown when Validation fails
+class ValidationError(Exception):
+	pass
+
+# schemas required for validating rugby conf files
+conf_vm_service_schema = {
+	'type' : '//rec',
+	'required' : {
+		'group': {'type': '//str'},
+		'type':  {'type': '//str'}
+	}
+}
+
+conf_vm_schema = {
+	'type': '//rec',
+	'required' : {
+   		'name': { 'type': '//str' },
+   		'service': conf_vm_service_schema
+	}
+}
+
+conf_schema = {
+	'type': '//arr',
+	'length' : {
+    	'min' : 1
+	},
+	'contents': conf_vm_schema    
+}
+
+# global object that validates rugby conf files
+rx = pyrx.Factory({'register_core_types': True})
+schema =  rx.make_schema(conf_schema)
 
 class RugbyLoader:
     """
@@ -14,28 +56,40 @@ class RugbyLoader:
         rugby_load = RugbyLoader('path/to/.rugby.yml')
         rugby_load.render_vagrant('/opt/VMs')
     
-    RugbyLoader globals
-        - rugby_conf: path to .rugby.yml
-        - rugby_obj: dictionary version of .rugby.yml
+    RugbyLoader static variables
+        - valid_groups: groups e
+        - valid_types: dictionary version of .rugby.yml
     """
-    rugby_conf = None
-    rugby_obj = None
 
-    def __init__(self, rugby_conf):
-        """
-        rugby_conf = path to .rugby.yml
+    def __init__(self, rugby_conf, **kwargs):
+	"""
+	Args:
+		rugby_conf (str) path to .rugby.yml
+	Kwargs:
+		static_ips (list) optionally pass a list static_ips 
+		to use for the provision vms
+	"""
+	self.rugby_conf = rugby_conf
 
-        """
-        self.rugby_conf = rugby_conf
-        
-        # Pull VM definitions from rugby_conf
-        vms = self._parse()
-        # Inject static ip for each VM definition
-        for vm in vms:
-            vm_group = vm['service']['group']
-            vm['ip'] = config.VM_STATIC_IPS[vm_group]
-        
-        self.rugby_obj = vms
+	if 'static_ips' in kwargs:
+		static_ips_list = kwargs['static_ips']
+		self.static_ips = dict(zip(valid_groups, static_ips_list))
+	else:
+		self.static_ips = config.VM_STATIC_IPS	
+
+	# Pull VM definitions from rugby_conf
+	vms = self._parse()
+
+	self.rugby_obj = vms
+
+	# Validate VM definitions object 
+	if not self._valid_config():
+		raise ValidationError(rugby_conf + " failed validation")
+
+	# Inject static ip for each VM definition
+	for vm in vms:
+		vm_group = vm['service']['group']
+		vm['ip'] = self.static_ips[vm_group]
 
     def render_vagrant(self, dest_dir):
         """
@@ -44,15 +98,38 @@ class RugbyLoader:
         """
         # Where vagrantfile will be generated
         generated_vagrantfile = os.path.join(dest_dir, 'Vagrantfile')
-        # Where vagrantfile template is located
-        vagrant_template_file = config.VAGRANT_TEMPLATE_FILE
-        
         with open(vagrant_template_file) as template_file:
-            j2_template = Template(template_file.read())
+            j2_template = jinja2.Template(template_file.read())
             # Write resulting Vagrantfile to dest_dir
             with open(generated_vagrantfile, 'w') as output_vagrantfile:
                 output_vagrantfile.write(j2_template.render(vms=self.rugby_obj, site_yml_path=config.SITE_YML))
     
+    def _validate_groups_and_types(self):
+	"""
+	Helper function which checks to see that valid group and type pair
+	in self.rugby_obj, returns True if it is a valid group and type pair
+	"""
+	for vm in self.rugby_obj:
+		vm_service = vm['service']
+		vm_group = vm_service['group']
+		vm_type = vm_service['type']
+		
+		valid_group = vm_group in valid_groups
+		valid_type = vm_type in valid_types 
+		valid_pair = (vm_group in valid_group_to_types and 
+					vm_type in valid_group_to_types[vm_group])
+		
+		if not valid_group or not valid_type or not valid_pair:
+			return False
+	return True
+
+    def _valid_config(self):
+	"""
+	Helper function which validates self.rugby_obj based on the
+	schema defined
+	"""
+	return schema.check(self.rugby_obj) and self._validate_groups_and_types()
+		
     def _parse(self):
         """
         Helper function which parses self.rugby_conf, and returns
@@ -63,4 +140,3 @@ class RugbyLoader:
             parsed_config_obj = yaml.safe_load(config_file)
         
         return parsed_config_obj
-
