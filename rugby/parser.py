f79a987ed11eefda75f9ea71be4d189fb5bc0501
Alex Gonzales <agonz056@ucr.edu>
2015-02-10 20:33:31 -0500

    updated parser arguments

--- rugby/parser.py
+++ rugby/parser.py
@@ -1,48 +1,48 @@
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
-
 RUGBY_YML = '.rugby.yml'
 
 VAGRANTFILE = 'Vagrantfile'
-
-TEMPLATE_NAME = VAGRANTFILE + '.template'
-TEMPLATE_PATH = 'template_path'
 
 """Parses a yaml config file and outputs a new
 Vagrantfile generated from a template.
 
 Args:
-    playbook_basepath (str): path where ansible playbooks are located
-    config_basepath (str): full path of the yaml config file
+    template_path (str): full path of the templated Vagrantfile
+        to generate a Vagrantfile
 
-Kwargs:
-    template_path (str): full path of the template file to generate a Vagrantfile from.
-        defaults to playbook_basepath + '/templates + TEMPLATE_NAME
+    site_yml_path (str): full path where site_yml (and the ansible playbooks) are located
+         should not be relative
+
+    config_basepath (str): directory where the yaml config file is located
+
+    output_basepath (str): directory where the final Vagrantfile ends up
+
+Example:
+
+        template_path = '../templates/Vagrantfile.template'
+        site_yml_path =  '../Rugby-Playbooks/site.yml'
+        config_basepath = '../repos/examples'
+        output_basepath = '../vm123'
+        parse(template_path, site_yml_path, config_basepath, output_basepath)
 
 """
-def parse(playbook_basepath, config_basepath, **kwargs):
+def parse(template_path, site_yml_path, config_basepath, output_basepath):
+    template_path_parts = template_path.split('/')
 
-    if TEMPLATE_PATH in kwargs:
-        template_path = kwargs[TEMPLATE_PATH]
-        template_path_parts = template_path.split('/')
-
-        template_basepath = '/'.join(template_path_parts[:-1])
-        template_name = template_path_parts[-1]
-    else:
-        # default template path
-        template_basepath = playbook_basepath + '/templates'
-        template_name = TEMPLATE_NAME
+    template_basepath = '/'.join(template_path_parts[:-1])
+    template_name = template_path_parts[-1]
 
     env = Environment(loader=FileSystemLoader(template_basepath))
     template = env.get_template(template_name)
 
-    config_path = playbook_basepath +'/' + RUGBY_YML
-    generated_vagrantfile_path = playbook_basepath + '/' + VAGRANTFILE
+    config_path = config_basepath +'/' + RUGBY_YML
+    generated_vagrantfile_path = output_basepath + '/' + VAGRANTFILE
 
     with open(config_path, 'r') as config_file:
         vms = yaml.safe_load(config_file)
         with open(generated_vagrantfile_path, 'w') as generated_vagrantfile:
-            template_output = template.render(vms = vms)
+            template_output = template.render(vms = vms, site_yml_path=site_yml_path)
             generated_vagrantfile.write(template_output)
 
