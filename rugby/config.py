65e2262f7b78fba4c343a6376c33a54056571e66
omoman <mannysanchez998@gmail.com>
2015-02-10 23:39:54 -0800

    Added templates dir and added templates dir to config

--- rugby/config.py
+++ rugby/config.py
@@ -1,7 +1,9 @@
-import os
+from os.path import dirname, abspath, join
 
 # Virtual machine related constants
 VM_BASE_DIR = '/opt/VMs'
 
 # Rugby constants
-SCRIPTS_DIR = os.path.dirname(os.path.abspath(__file__)) + '/shell_scripts'
+SCRIPTS_DIR = dirname(abspath(join(__file__, 'shell_scripts')))
+TEMPLATES_DIR = dirname(abspath(join(__file__, 'templates')))
+
