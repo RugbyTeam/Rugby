6b5cb515e000ecdddaa8b123bca45c1ad94426e9
omoman <mannysanchez998@gmail.com>
2015-02-13 23:26:42 -0800

    Removed unused shell scripts

--- rugby/shell_scripts/run_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This script executes a command on a VM and returns its output
-# Example Command:
-# ansible MongoDB -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --private-key=.vagrant/machines/MongoDB/virtualbox/private_key -u vagrant -m shell -a "~/break.sh"
-
-echo "This script does nothing yet..."
