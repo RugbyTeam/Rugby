dae098e199d622df0ceaecf2bcc32b915e2c794f
omoman <mannysanchez998@gmail.com>
2015-02-15 16:20:21 -0800

    Rugby worker is in a usable state now

--- rugby/config.py
+++ rugby/config.py
@@ -1,8 +1,6 @@
 # stdlib
 from os.path import dirname, abspath, join
 
-# Virtual machine related constants
-VM_BASE_DIR = '/opt/VMs'
 VM_STATIC_IPS = {
     'db'   : '192.168.0.15',
     'lang' : '192.168.0.16'
@@ -16,11 +14,10 @@ GROUP_TO_TYPES = {
 TYPES = sorted({vm_type for vm_types in GROUP_TO_TYPES.itervalues() for vm_type in vm_types})
 
 # Rugby constants
+BASE_DIR = '/opt/VMs'
 SITE_YML = '/opt/Rugby-Playbooks/site.yml'
-SCRIPTS_DIR = abspath(join(dirname(__file__), 'shell_scripts'))
 TEMPLATES_DIR = abspath(join(dirname(__file__), 'templates'))
 VAGRANT_TEMPLATE_FILE = join(TEMPLATES_DIR, 'Vagrantfile.j2')
-VAGRANT_WRAPPER_SCRIPT = abspath(join(SCRIPTS_DIR, 'vagrant_wrapper.sh'))
 
 # Logger constants
 LOGGER_NAME = 'rugby-console'
--- rugby/rugby.py
+++ rugby/rugby.py
@@ -1,109 +1,55 @@
 # internal
-from rugby_loader import RugbyLoader
+from rugby_worker import RugbyWorker
 import config
 
-# external
-from enum import Enum
-
 # stdlib
-from subprocess import Popen
+from multiprocessing import Process, Pipe
 import logging
-import threading
-import os
 
 logger = logging.getLogger(config.LOGGER_NAME)
 
-class RugbyState(Enum):
-    standby = 1
-    provisioning = 2
-    running = 3
-    success = 4
-    fail = 5
+class WorkerInfo:
+    """
+    Struct to hold all the info we need about a RugbyWorker
+    """
+    def __init__(self, worker_pid, worker_msg_pipe):
+        self.pid = worker_pid
+        self.msg_pipe = worker_msg_pipe
+
+        # Will be set as Worker starts to work (haha)
+        self.state = None
 
 class Rugby:
     """
     Rugby Globals
-        - tasks: dictionary that holds all Rugby tasks referenced by commit ID 
+        - workers: Dictionary of all rugby workers
+            { "<commit_id>" : {"worker" : <WorkerInfo>} }
     """
-    tasks = {}
+    workers = {}
 
-    """
-    Rugby Methods
-    """
-    def __init__(self, rugby_root):
+    def __init__(self, rugby_root=config.BASE_DIR):
+        """
+        rugby_root = Directory where all rugby generated files
+                     should be placed
+        """
         self.rugby_root = rugby_root
 
-    def up(self, commit_id, rugby_config):
-        rt = RugbyTask(commit_id, self.rugby_root, rugby_config)
-        Rugby.tasks[commit_id] = rt
-        rt.provision()
+    def start_runner(self, commit_id, rugby_config):
+        """
+        Method takes a unique commit_id and a path (rugby_config)
+        to a rugby config file and creates a rugby worker which will 
+        execute all the instructions in the config
+        """
+        # Instantiate a worker
+        rw = RugbyWorker(commit_id, self.rugby_root, rugby_config)
+        
+        # Create pipe for interprocess communication
+        my_end, their_end = Pipe()
 
-    def status(self):
-        return Rugby.tasks
+        # Start worker process
+        worker_process = Process(target=rw, args=(their_end,))
+        worker_process.start()
 
-class RugbyTask:
-    """
-    RugbyTask Globals
-        - task_pool: list that holds currently running task subprocesses
-    """
-    task_pool = []
-
-    """
-    RugbyTask Methods
-    """
-    def __init__(self, commit_id, rugby_root, rugby_config):
-        self.commit_id = commit_id
-        self.vagrant_cwd = os.path.join(rugby_root, commit_id)
-        self.rugby_config = rugby_config
-        self.state = RugbyState.standby
-
-    def provision(self):
-        self.state = RugbyState.provisioning
-
-        try:
-            if not os.path.exists(self.vagrant_cwd):
-                os.makedirs(self.vagrant_cwd)
-
-            #TODO: Parse rugby_config here and place in vagrant_cwd
-            # set state to fail if parse failed
-            
-            rugby_load = RugbyLoader(self.rugby_config)
-            rugby_load.render_vagrant(self.vagrant_cwd)
-        except OSError:
-            logger.error("RugbyTask Provisioning OSError")
-
-        # Start the VMs
-        logfile = os.path.join(self.vagrant_cwd, '%s.log' % self.commit_id)
-        with open(logfile, 'a') as f:
-            self.pproc = Popen([config.VAGRANT_WRAPPER_SCRIPT, self.vagrant_cwd, "up"], stdout=f, stderr=f)
-            RugbyTask.task_pool.append(self)
-
-    def notify(self):
-        # Analyze provisioning status
-        if self.pproc.poll() != 0:
-            self.state = RugbyState.fail
-            logger.error("Provisioning Process Failed")
-        else:
-            self.run_all_tests()
-
-    #TODO: Define how tests work... how they are referenced and run
-    def run_test(self, test):
-        logger.info("{} running test {}".format(self.commit_id, test))
-
-    def run_all_tests(self):
-        logger.info("{} running all tests".format(self.commit_id))
-
-# Start up subprocess poller
-def task_poller():
-    while True:
-        for task in RugbyTask.task_pool:
-            if task.pproc.poll() != None:
-                logger.debug("Process Terminated... {} Exit code: {}".format(task.pproc.poll(), task.pproc))
-                RugbyTask.task_pool.remove(task)
-                task.notify()
-
-t = threading.Thread(target=task_poller)
-t.daemon = True
-t.start()
-
-
+        # Record worker info
+        worker_info = WorkerInfo(worker_process.pid, my_end)
+        Rugby.workers[commit_id] = worker_info
